dc4de43249f5b2aea91b9d58ee18f8793da72f2f
AlexDuOl <alexdurancm@gmail.com>
2020-12-30 23:19:20 -0600

    Inicializando repo

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Importando las librerias React y ReactDOM
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Creando componentes de React
+const App = () => {
+    return <div>Hola!!</div>
+};
+
+// Tomando y mostrando los componentes de React en la pantalla
+ReactDOM.render(<App />, document.querySelector('#root'));
